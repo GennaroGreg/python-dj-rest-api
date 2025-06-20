6f2d9d41a72ffdbba038b71de9cfaa10a190ef78
GennaroGreg <gennarogreg@gmail.com>
2025-06-20 15:17:05 -0700

    Pushing initial functionality

---
+++ rest_api_demo/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializer import BookSerializer
+
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_book(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
